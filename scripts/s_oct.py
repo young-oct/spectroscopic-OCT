23cb44bedf6d902acf707e73c17b457d15dbe757
young-oct <>
2022-09-03 07:28:56 -0300

    included standard b mode processing

--- scripts/s_oct.py
+++ scripts/s_oct.py
@@ -17,6 +17,11 @@ from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import matplotlib
+
+def Aline_R(data,start):
+    A_line = ifft(data, axis=1)
+    return A_line[dwell * start:dwell * (start + 512), -350:-20].T
 
 def load_raw(file_path):
     if Path(file_path).is_file():
@@ -25,6 +30,41 @@ def load_raw(file_path):
         raw = temp['arr_1']
         return raw
 
+def mean_remove(s,decimation_factor):
+    s = s - np.mean(s, axis=1)[:, np.newaxis]
+    # (3) sample every decimation_factor line,
+    s = s[:, ::decimation_factor]
+    return s
+
+dwell = 20
+
+
 if __name__ == '__main__':
+
+    matplotlib.rcParams.update(
+        {
+            'font.size': 16,
+            'text.usetex': False,
+            'font.family': 'sans-serif',
+            'mathtext.fontset': 'stix',
+        }
+    )
+
     file_path = '../data/finger(raw).npz'
-    a = load_raw(file_path)
+    raw = load_raw(file_path)
+    start, decimation_factor = 420, 20
+    rvmin, vmax = 15, 55  # dB
+
+    s_r = mean_remove(Aline_R(raw, start), decimation_factor)
+    sr_log = 20 * np.log10(abs(s_r))
+
+    fig = plt.figure(figsize=(16, 9),constrained_layout=True)
+
+    gs = fig.add_gridspec(ncols=2, nrows=1)
+    ax = fig.add_subplot(gs[0])
+    ax.set_title('(a) standard B-mode')
+
+    ax.imshow(sr_log, 'gray',
+              aspect=sr_log.shape[1] / sr_log.shape[0],
+              vmin=rvmin, vmax=vmax, interpolation='none')
+    plt.show()
